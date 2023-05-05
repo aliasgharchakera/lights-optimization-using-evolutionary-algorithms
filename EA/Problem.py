e78236f2c9d9c3a028c22a9cb1bba416cb640d3f
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2023-05-06 04:51:23 +0500

    EA STRUCTURE ADDED !

---
+++ EA/Problem.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+# Abstract class for any optimization problem
+class Problem(ABC):
+    """
+    Abstract class for any optimization problem
+
+    Args:
+        ABC (): Abstract Base Class
+
+    """
+
+    # inverse_fitness: bool = False # variable used in plotting BSF & ASF graph, use if needed 
+
+    @abstractmethod
+    def chromosome(self) -> list:
+        """Return a random chromosome
+
+        Returns:
+            list: Random chromosome
+        """
+        pass
+
+    @abstractmethod
+    def fitness_function(self) -> float:
+        """Return the fitness of a chromosome
+
+        Returns:
+            float: Fitness of a chromosome
+        """
+        pass
+
+    @abstractmethod
+    def crossover(self):
+        """Return a crossovered chromosome
+        """
+        pass
+
+    @abstractmethod
+    def mutate(self) -> list:
+        """Return a mutated chromosome
+
+        Returns:
+            list: Mutated chromosome
+        """
+        pass
+
