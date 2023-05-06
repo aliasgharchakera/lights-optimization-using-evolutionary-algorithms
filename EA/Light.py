17acaf87c1f407e55e04f05870a7f4dc35f43f88
Syed Ibrahim Ali <sh06565@st.habib.edu.pk>
2023-05-06 16:26:27 +0500

    lights GA updated

--- EA/Light.py
+++ EA/Light.py
@@ -41,30 +41,44 @@ class Light(Problem):
         Returns:
             list: offspring after breeding from two parents
         """
+
+        # select two random genes from parents
         gene1 = int(random.random() * len(parent1))
         gene2 = int(random.random() * len(parent1))
 
+        # find the start and end gene for breeding
         start_gene = min(gene1, gene2)
         end_gene = max(gene1, gene2)
+
+        # create child 1 by taking genes from start_gene to end_gene from parent1
         child1 = parent1[start_gene:end_gene]
+
+        # create child 2 by taking genes not present in child1 from parent2
         child2 = [gene for gene in parent2 if gene not in child1]
+
+        # concatenate child1 and child2 to create the final offspring
         child = child1 + child2
         return child
 
     # MUTATE COMPLETE 
     @staticmethod
     def mutate(individual: list) -> list:
-        """Mutates the route by swapping two cities
+        """Mutates the ----- by swapping two -----
 
         Args:
-            individual (list): list of cities
+            individual (list): list of -----
 
         Returns:
-            list: list of cities after mutation
+            list: list of ---- after mutation
         """
+
+        # select two random indexes to swap in the individual chromosome
         indexes = random.sample(list(range(len(individual))), 2)
+
+         # swap the two chromosones at the selected indexes
         swap1, swap2 = indexes[0], indexes[1]
-        individual[swap1], individual[swap2] = individual[swap2], individual[
-            swap1]
+        individual[swap1], individual[swap2] = individual[swap2], individual[swap1]
+
+        # return mutated choromose
         return individual
 
