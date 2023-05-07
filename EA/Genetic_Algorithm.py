9cf309625a6675ebfdb8f344fa7d2be1f6145ed2
princebiscuitsm <aliasghar.chakera@gmail.com>
2023-05-08 03:46:01 +0500

    GA __name__

--- EA/Genetic_Algorithm.py
+++ EA/Genetic_Algorithm.py
@@ -333,21 +333,22 @@ selection_methods = [
 4,4 99,13,43
 
 """
-opt = GA(
-    problem=Light,
-    X = 10,
-    Y = 10,
-    H = 50,
-    room = Room(100,100,50,[(0,0,2,45),(5,5,2,42),(1,9,2,10),(3,9,1,4)],2,[(0,0,0,0)]),
-    parent_selection = 3,
-    survivor_selection = 2,
-    population_size=30,
-    number_of_offsprings=10,
-    number_of_generations=100,
-    mutation_rate=0.50
-    )
+if __name__ == "__main__":
+    opt = GA(
+        problem=Light,
+        X = 10,
+        Y = 10,
+        H = 50,
+        room = Room(100,100,50,[(0,0,2,45),(5,5,2,42),(1,9,2,10),(3,9,1,4)],2,[(0,0,0,0)]),
+        parent_selection = 3,
+        survivor_selection = 2,
+        population_size=30,
+        number_of_offsprings=10,
+        number_of_generations=100,
+        mutation_rate=0.50
+        )
 
-population = (opt.initial_population())
+    population = (opt.initial_population())
 
 # fitness, population, tiles= (opt.run())
 # print(fitness, "\n",population,"\n", tiles)
