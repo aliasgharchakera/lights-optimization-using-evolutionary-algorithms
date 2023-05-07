e35a0e980bf9a17dc514c8f919133e83002bdc47
Hafsa <hk06665@st.habib.edu.pk>
2023-05-07 20:55:39 +0500

    visualisation

--- Visualization/game_2.py
+++ Visualization/game_2.py
@@ -1,107 +1,115 @@
 import pygame
-from pygame.locals import *
+import sys
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import time
+
 
 # Initialize Pygame
 pygame.init()
 
-# Define the dimensions of the window
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+clock = pygame.time.Clock()
 
-# Create the window
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+# Set the size of the window
+WINDOW_SIZE = (800, 600)
+screen = pygame.display.set_mode(WINDOW_SIZE, pygame.DOUBLEBUF|pygame.OPENGL)
 
-# Define the colors
+# Set up OpenGL
+glViewport(0, 0, WINDOW_SIZE[0], WINDOW_SIZE[1])
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluPerspective(45, (WINDOW_SIZE[0] / WINDOW_SIZE[1]), 0.1, 50.0)
+glMatrixMode(GL_MODELVIEW)
+glEnable(GL_DEPTH_TEST)
+
+# Define some colors
+WHITE = (1, 1, 1)
+GRAY = (0.5, 0.5, 0.5)
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
+RED = (1, 0, 0)
+GREEN = (0, 1, 0)
+BLUE = (0, 0, 1)
 
-LIT = (0,0,0)
-UNLIT = (255,255,255)
+# Define some constants
+TILE_SIZE = 1
+FLOOR_WIDTH = 10
+FLOOR_HEIGHT = 10
+WALL_HEIGHT = 5
+ROOM_WIDTH = FLOOR_WIDTH * TILE_SIZE
+ROOM_HEIGHT = WALL_HEIGHT * TILE_SIZE
+ROOM_DEPTH = FLOOR_HEIGHT * TILE_SIZE
+# colours 
+lit_color = (1, 1, 1)
+unlit_color = (0.2, 0.2, 0.2)
+def partial_color(color, intensity):
+    return (color[0] * intensity, color[1] * intensity, color[2] * intensity)
+partial_lit = partial_color(lit_color, 0.5)
+# # Define the nested list that represents the floor
+# floor = [[(0.647, 0.165, 0.165) for j in range(FLOOR_HEIGHT)] for i in range(FLOOR_WIDTH)]
+# floor[2][3] = (1, 1, 1)  # Set a tile to be lit
+# floor[4][7] = (0.4, 0.4, 0.4)  # Set a tile to be less intensely lit
+# floor[6][2] = (0.8, 0.8, 0.8)  # Set a tile to be partially lit
+# floor[8][5] = (0.2, 0.2, 0.2)  # Set a tile to be unlit
 
-def partially_lit_color(intensity):
-    return (255-intensity*255,255-intensity*255,255-intensity*255)
+# Define a function to draw a tile
+def draw_tile(tile_x, tile_y, color):
+    glBegin(GL_QUADS)
+    glColor3f(*color)
+    glVertex3f(tile_x, tile_y, 0)
+    glVertex3f(tile_x + TILE_SIZE, tile_y, 0)
+    glVertex3f(tile_x + TILE_SIZE, tile_y + TILE_SIZE, 0)
+    glVertex3f(tile_x, tile_y + TILE_SIZE, 0)
+    glEnd()
 
-# Define the dimensions of the room
-ROOM_WIDTH = 600
-ROOM_HEIGHT = 400
-ROOM_DEPTH = 300
-
-# Define the dimensions of the grid
-GRID_WIDTH = 20
-GRID_HEIGHT = 20
-
-# Define the position of the camera
-camera_x = 0
-camera_y = 0
-camera_z = -50
-camera_angle_x = 0
-camera_angle_y = 0
-
-# Define the position of the lights
-lights = [[False for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
-
-# Define a function to draw the room
-def draw_room():
-    # Draw the floor
-    pygame.draw.rect(screen, WHITE, (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2, ROOM_WIDTH, ROOM_HEIGHT))
-    
-    # Draw the ceiling
-    pygame.draw.rect(screen, BLACK, (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2, ROOM_WIDTH, ROOM_HEIGHT//2))
-    
-    # Draw the walls
-    pygame.draw.line(screen, BLACK, (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2), (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+ROOM_DEPTH))
-    pygame.draw.line(screen, BLACK, (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2), (WINDOW_WIDTH//2+ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2))
-    pygame.draw.line(screen, BLACK, (WINDOW_WIDTH//2+ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2), (WINDOW_WIDTH//2+ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+ROOM_DEPTH))
-    pygame.draw.line(screen, BLACK, (WINDOW_WIDTH//2-ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+ROOM_DEPTH), (WINDOW_WIDTH//2+ROOM_WIDTH//2, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+ROOM_DEPTH))
-    
-    # Draw the grid
-    for i in range(GRID_WIDTH):
-        for j in range(GRID_HEIGHT):
-            if lights[i][j]:
-                pygame.draw.rect(screen, YELLOW, (WINDOW_WIDTH//2-ROOM_WIDTH//2+i*GRID_WIDTH, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+j*GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT))
+# Define a function to draw the floor
+def draw_floor(floor):
+    for i in range(FLOOR_WIDTH):
+        for j in range(FLOOR_HEIGHT):
+            tile_x = i * TILE_SIZE - ROOM_WIDTH/2
+            tile_y = j * TILE_SIZE - ROOM_DEPTH/2
+            if floor[i][j].give_status() == True:
+                color = lit_color
+            elif floor[i][j].intensity > 0:
+                color = partial_color(lit_color, floor[i][j].intensity)
+            elif sum(floor[i][j].fill_vessel) > 0:
+                color = partial_lit
             else:
-                pygame.draw.rect(screen, BLACK, (WINDOW_WIDTH//2-ROOM_WIDTH//2+i*GRID_WIDTH, WINDOW_HEIGHT//2-ROOM_HEIGHT//2+j*GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT))
-    
-# Define a function to handle events
-def handle_events():
-    global camera_x, camera_y, camera_z, camera_angle_x, camera_angle_y, lights
-    
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            quit()
-        elif event.type == KEYDOWN:
-            if event.key == K_UP:
-                camera_angle_x += 5
-            elif event.key == K_DOWN:
-                camera_angle_x -= 5
-            elif event.key == K_LEFT:
-                camera_angle_y += 5
-            elif event.key == K_RIGHT:
-                camera_angle_y -= 5
-            elif event.key == K_w:
-                camera_z += 5
-            elif event.key == K_s:
-                camera_z -= 5
-            elif event.key == K_a:
-                camera_x -= 5
-            elif event.key == K_d:
-                camera_x += 5
-            elif event.key == K_SPACE:
-                lights[0][0] = not lights[0][0]
-# Start the game loop
-while True:
-    # Handle events
-    handle_events()
+                color = unlit_color
+            
+            # color = floor[i][j]
+            draw_tile(tile_x, tile_y, color)
 
-    # Draw the scene
-    draw_room()
+# Main game loop
+def main_game_loop(floor_list):
+    count_max = len(floor_list)
+    count = 0
+    while True:
+        # Update the floor
+        floor = floor_list[count]
 
-    # Update the display
-    pygame.display.flip()
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    # Wait for a short amount of time to control the frame rate
-    pygame.time.wait(10)
+        # Clear the screen and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+        # Set up the camera
+        glLoadIdentity()
+        gluLookAt(0, -10, 5, 0, 0, 0, 0, 0, 1)
+
+        # Draw the floor
+        draw_floor(floor)
+
+        # Update the display
+        pygame.display.flip()
+        
+        time.sleep(0.5)
+
+        # Limit the framerate to 60 FPS
+        clock.tick(60)
+        if count < count_max:
+            count += 1
+# main_game_loop()
